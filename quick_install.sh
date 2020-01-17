a44f5defdd21ba9fe6ad0328d15fc7568300ea58
Edward Guan <285006386@qq.com>
2020-01-17 09:32:10 +0800

    update

--- quick_install.sh
+++ quick_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 helm repo update
-helm install stable/prometheus-operator -n prometheus-operator -f helm-values/prometheus-operator-values.yml --namespace kube-ops
-helm install stable/prometheus-adapter -n prometheus-adapter -f helm-values/prometheus-adapter-values.yml --namespace kube-ops
+helm upgrade -i prometheus-operator stable/prometheus-operator -f helm-values/prometheus-operator-values.yml --namespace kube-ops
+helm upgrade -i prometheus-adapter stable/prometheus-adapter -f helm-values/prometheus-adapter-values.yml --namespace kube-ops
 kubectl apply -f manifests/
